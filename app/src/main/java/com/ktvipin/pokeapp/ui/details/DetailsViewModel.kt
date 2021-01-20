4ef0068d41ddf847b4d3ebca36a33481f6b22874
vipinkt <ktvipin27@gmail.com>
2021-01-20 12:54:52 +0530

    abilities added

--- app/src/main/java/com/ktvipin/pokeapp/ui/details/DetailsViewModel.kt
+++ app/src/main/java/com/ktvipin/pokeapp/ui/details/DetailsViewModel.kt
@@ -3,11 +3,13 @@ package com.ktvipin.pokeapp.ui.details
 import android.os.Bundle
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import com.ktvipin.pokeapp.data.model.Ability
 import com.ktvipin.pokeapp.data.model.Details
 import com.ktvipin.pokeapp.data.model.PokeMon
 import com.ktvipin.pokeapp.data.repository.PokemonRepository
 import com.ktvipin.pokeapp.ui.base.viewmodel.BaseViewModel
 import com.ktvipin.pokeapp.ui.details.DetailsActivity.Companion.EXTRA_DATA
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -15,6 +17,8 @@ class DetailsViewModel @Inject constructor(private val pokemonRepository: Pokemo
     BaseViewModel() {
 
      val pokemonDetails = MutableLiveData<Details>()
+     val abilities = MutableLiveData<List<Ability>>()
+     val abilitiesString = MutableLiveData<String>()
 
     val pokeMon = MutableLiveData<PokeMon>()
 
@@ -24,10 +28,21 @@ class DetailsViewModel @Inject constructor(private val pokemonRepository: Pokemo
            val pokeMonData = it.get(EXTRA_DATA) as PokeMon
             this.pokeMon.value = pokeMonData
 
-            viewModelScope.launch {
-                val details = pokemonRepository.getDetails(pokeMonData.url)
-                pokemonDetails.value = details
+            fetData(pokeMonData)
+        }
+    }
+
+    private fun fetData(pokeMonData: PokeMon): Job {
+        return viewModelScope.launch {
+            val details = pokemonRepository.getDetails(pokeMonData.url)
+            pokemonDetails.value = details
+
+            val abilityList = mutableListOf<Ability>()
+            details.abilities.forEach {
+                abilityList.add(it.ability)
             }
+            abilitiesString.value = abilityList.map { it.name }.joinToString("\n")
+            abilities.value = abilityList
         }
     }
 }
